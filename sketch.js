fbfd5881a7e855f5ed3f2a7f4966c981256f90ca
yannndesign <110218906+yannndesign@users.noreply.github.com>
2023-01-11 17:34:42 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ let font2;
 
 function setup() {
 //   createCanvas(windowWidth-40, 700+(windowWidth-1700)*0.3);
-  createCanvas(windowWidth-40, 700+(windowWidth-1700)*0.3).parent('vb_logo_p5');
+  createCanvas(windowWidth-40, 700+(windowWidth-1700)*0.3).parent('p52');
 
   first = createVector(0, 0);
   second= createVector(0, 0);
